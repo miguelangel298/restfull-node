28a234697b698f986aa29652d37e47ae6e3db7f2
Miguel Hernandez <mhernandez@unphu.edu.do>
2019-01-15 10:26:27 -0400

    delete cosole

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.use(bodyParser.json());
 const port = process.env.port || 3000;
 const bookRouter = require('./application/routes/bookRoute')();
 
-console.log('esta mierdad son las routes' + bookRouter);
 app.use('/api', bookRouter);
 
 app.get('/', function(req, res) {
